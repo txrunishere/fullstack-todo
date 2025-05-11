738ead415384393d6e03519f0d10cf9511b88086
txrunishere <tsoni9286@gmail.com>
2025-05-11 19:09:41 +0530

    add CORS middleware to enable cross-origin requests from the frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({
   extended: true
+}))
+app.use(cors({
+  origin: "http://localhost:3000"
 }))
 
 app.use(cookieParser())
