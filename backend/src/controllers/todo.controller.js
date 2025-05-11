68c1ccf445cfc92390cecf8b56ff3470bff6f1e6
txrunishere <tsoni9286@gmail.com>
2025-05-11 11:40:11 +0530

    add complete and delete functionality for todos

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -38,4 +38,55 @@ const handleTodo = asyncHandler(async (req, res) => {
   }
 });
 
-export { handleTodo };
+const handleCompleteTodo = asyncHandler(async (req, res) => {
+  const { todoId } = req.params;
+
+  if (!todoId) {
+    return res.status(400).json({
+      error: "Todo ID is required",
+    });
+  }
+
+  const todo = await Todo.findByIdAndUpdate(todoId, {
+    isCompleted: true,
+  });
+
+  if (todo) {
+    return res.status(200).json({
+      message: "Todo marked as completed successfully",
+    });
+  } else {
+    return res.status(404).json({
+      error: "Todo not found",
+    });
+  }
+});
+
+const handleDeleteTodo = asyncHandler(async (req, res) => {
+  const { todoId } = req.params;
+
+  if (!todoId) {
+    return res.status(400).json({
+      error: "Todo ID is required",
+    });
+  }
+
+  const todo = await Todo.findByIdAndDelete(todoId);
+  const user = await User.findByIdAndUpdate(req.user?._id, {
+    $pull: {
+      todo: todo._id,
+    },
+  });
+
+  if (todo && user) {
+    return res.status(200).json({
+      message: "Todo deleted successfully",
+    });
+  } else {
+    return res.status(500).json({
+      error: "Failed to delete todo",
+    });
+  }
+});
+
+export { handleTodo, handleCompleteTodo, handleDeleteTodo };
--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { handleTodo } from '../controllers/todo.controller.js'
+import { handleTodo, handleCompleteTodo, handleDeleteTodo } from '../controllers/todo.controller.js'
 import { verifyAuth } from '../middleware/auth.middleware.js'
 const router = Router()
 
 router.use(verifyAuth)
 
 router.route('/create-todo').post(handleTodo)
+router.route('/complete/:todoId').patch(handleCompleteTodo)
+router.route('/delete/:todoId').delete(handleDeleteTodo)
 
 export default router
